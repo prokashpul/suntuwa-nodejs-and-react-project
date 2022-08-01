9433a1d9a623d53b52a0dfbed65ac65fc0d751a6
prokash <pprokash102@gmail.com>
2022-08-01 21:53:10 +0600

    server side mongodb add and connect and client side recat router and tailwincss install

--- server site/_staps.js
+++ server site/_staps.js
@@ -3,5 +3,6 @@
  * Express & cors install
  * create index.js
  * install modemon
- *
+ * install dotenv
+ *install mongodb
  */
--- server site/index.js
+++ server site/index.js
@@ -1,11 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
+//mongodb user: santuwa
+//password : 0e3GhtaD2Av5Hsrr
+//mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jpceo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+    await client.connect();
+
+    const toolsCollection = client.db("santuwa").collection("user");
+    const user = {
+      name: "Prokash Pul",
+      email: "prokashpul2@gmail.com",
+    };
+    const result = await toolsCollection.insertOne(user);
+    console.log(result.insertedId);
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 // get api
 app.get("/", async (req, res) => {
   res.send("Hello world ");
